b85ad6ea4e76079d2800bf671e6cd606aedffed2
Kseniia-nik <kseniadnikolaeva@gmail.com>
2025-05-02 12:07:57 +1000

    Update app.py

--- app.py
+++ app.py
@@ -47,13 +47,17 @@ h3 {{
 </style>
 """, unsafe_allow_html=True)
 
-# === HEADER ===
-col1, col2 = st.columns([0.85, 0.15])
+# === HEADER: TITLE + LOGO ===
+col1, col2, col3 = st.columns([0.7, 0.15, 0.15])
+
 with col1:
     st.markdown("<h1>Fintelligen</h1>", unsafe_allow_html=True)
     st.markdown("<h3>AI Resume Evaluator for Goldman Sachs</h3>", unsafe_allow_html=True)
 with col2:
-    st.image("goldman.jpeg", width=80)
+    st.image("goldman.jpeg", width=70)
+with col3:
+    st.image("FINTELLIGEN.svg", width=70)
+
 
 # === INSTRUCTIONS ===
 with st.expander("📋 Instructions for HR", expanded=True):
