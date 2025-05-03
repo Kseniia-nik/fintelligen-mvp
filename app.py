2dec3aaef9a703ff77717aa725af0419c3a70187
Kseniia-nik <kseniadnikolaeva@gmail.com>
2025-05-03 23:11:37 +1000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 
 # === THEME COLORS ===
 accent_color    = "#473BD0"
-highlight_color = "#c59d5f"
+highlight_color = "#473BD0"
 bg_color        = "#f8f9fa"
 text_color      = "#212529"
 card_color      = "#ffffff"
